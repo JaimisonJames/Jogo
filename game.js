4e6f0d0db641fb24e4eb5d237d6eaf3e20752284
JaimisonJames <37158234+JaimisonJames@users.noreply.github.com>
2018-03-16 12:49:32 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+     el: '#app',
+     data: {
+      ptsVidaJogador: 100,
+      ptsVidaOponente: 100,
+      vidaAtualJogador: 0,
+      vidaAtualOponente: 0,
+      numAtaqueJogador: 0,
+      numAleatorioCurar: 0,
+      numAtaqueOponente: 0,
+    },
+
+methods:{
+  ataque: function () {
+        this.vidaAtualJogador = this.ptsVidaJogador;
+        this.vidaAtualOponente = this.ptsVidaOponente;
+    if (this.ptsVidaOponente > 0 && this.ptsVidaJogador > 0) {
+        this.numAtaqueJogador = Math.floor(Math.random() * 9) + 2;
+        this.numAtaqueOponente = Math.floor(Math.random() * 8) + 1;
+        this.ptsVidaJogador = this.ptsVidaJogador - this.numAtaqueOponente;
+        this.ptsVidaOponente = this.ptsVidaOponente - this.numAtaqueJogador;
+      }
+    },
+
+  recuperar: function() {
+    if (this.ptsVidaJogador + this.numAleatorioCurar < 100) {
+        this.numAtaqueOponente = Math.floor(Math.random() * 8) + 1;
+        this.numAleatorioCurar = Math.floor(Math.random() * 5) + 3;
+        this.ptsVidaJogador = this.ptsVidaJogador + this.numAleatorioCurar;
+        this.ptsVidaJogador = this.ptsVidaJogador - this.numAtaqueOponente;
+      }
+    },
+
+  reiniciar: function() {
+        this.numAtaqueJogador = 0;
+        this.numAleatorioCurar = 0;
+        this.numAtaqueOponente = 0;
+        this.ptsVidaJogador = 100;
+        this.ptsVidaOponente = 100;
+      }
+    },
+});
